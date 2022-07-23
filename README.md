34adc56a00911e63a56ab387ff9a95d1f22b17c9
川口涼一 <kawaguchi.ryo.0802@gmail.com>
2022-07-23 17:09:30 +0900

    read me img add

--- README.md
+++ README.md
@@ -1,5 +1,45 @@
 # param-check
-URLのパラメータに不備が無いかチェックします。 Check the URL parameters for any deficiencies.
+
+<br>
+
+URLのパラメータに不備が無いかチェックします。
+
+Check the URL parameters for any deficiencies.
+
+<br>
+
+---
+
+<br>
+
+## example
+
+![param-check01](https://user-images.githubusercontent.com/46624509/180596159-eb0a6296-5af0-4602-a1a7-c941041f8d4e.jpg)
+
+<br>
+
+"https://github.com/" と入力した場合
+
+If you enter "https://github.com/".
+
+![param-check02](https://user-images.githubusercontent.com/46624509/180596313-834a6a1e-f7f5-438f-9883-240714cfa794.jpg)
+
+"https://github.com/?test" と入力した場合
+
+If you enter "https://github.com/?test".
+
+![param-check03](https://user-images.githubusercontent.com/46624509/180596421-a45ba1b0-a220-4f9a-a511-fa081c50fcfd.jpg)
+
+"https://github.com/&test" と入力した場合
+
+If you enter "https://github.com/&test".
+
+![param-check04](https://user-images.githubusercontent.com/46624509/180596527-a57ca336-6539-4107-958f-17d8c306df6d.jpg)
+
+---
 
 ## setting
-チェックのバリデートの設定は「popup.js」を編集してください。　 Edit "popup.js" to set the validation of the check.
+
+チェックのバリデートの設定は「popup.js」を編集してください。　
+
+Edit "popup.js" to set the validation of the check.
--- src/js/popup.js
+++ src/js/popup.js
@@ -22,7 +22,7 @@ const func = () => {
     textForm.value = replacedValue;
   }, false);
 
-  // 「チェック」ボタンを押して入力欄を取得する
+  // 「CHECK」ボタンを押して入力欄を取得する
   const buttonClick = () => {
     const matchInput = document.getElementById('match'),
     matchValue = document.getElementById('match').value,
@@ -36,48 +36,48 @@ const func = () => {
       };
 
     // 条件分岐
-    // 入力欄が "空" の場合
+    // 入力欄が "空" の場合 = NG
     if (matchValue === "") {
       const validateMsg = `
-        <span class="alert--text">URLを入力して下さい</span>`;
+        <span class="alert--text">Please enter the URL.</span>`;
       validate(validateMsg);
       validateAlertClassRepeat();
     }
 
-    // "?" が2つ以上含まれている場合 / "/&" 始まりの場合
+    // "?" が2つ以上含まれている場合 / "/&" 始まりの場合 = NG
     else if (regexpParam01.test(matchValue) || regexpParam02.test(matchValue)) {
       const validateMsg = `
-        <span class="alert--text">不正なパラメータが含まれています</span>`;
+        <span class="alert--text">It contains invalid parameters.</span>`;
       validate(validateMsg);
       validateAlertClassRepeat();
     }
 
-    // "?" 始まりの場合
+    // "?" 始まりの場合 = OK
     else if (regexpParam03.test(matchValue) || regexpParam04.test(matchValue)) {
       const validateMsg = `
-        <span class="success--text">正規のパラメータです</span>`;
+        <span class="success--text">This is the correct parameter.</span>`;
       validate(validateMsg);
       matchInput.classList.remove('alert--bg');
     }
 
-    // "&" 始まりの場合
+    // "&" 始まりの場合 = NG
     else if (regexpParam05.test(matchValue)) {
       const validateMsg = `
-        <span class="alert--text">不正なパラメータが含まれています</span>`;
+        <span class="alert--text">It contains invalid parameters.</span>`;
       validate(validateMsg);
       validateAlertClassRepeat();
     }
 
-    // 上記以外
+    // 上記以外 = NG
     else {
       const validateMsg = `
-      <span class="alert--text">正しいURLを入力して下さい</span>`;
+      <span class="alert--text">Please enter the correct URL.</span>`;
       validate(validateMsg);
       validateAlertClassRepeat();
     }
   }
 
-  // 「チェック」ボタンを押した際の動作
+  // 「CHECK」ボタンを押した際の動作
   const button = document.getElementById('form__submit');
   button.addEventListener('click', buttonClick);
 }
